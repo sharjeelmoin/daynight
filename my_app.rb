9f811e0091e5d393629de2982228ac30ff9e7f11
Ali Taha <ali.taha@inheritedarts.com>
2017-02-17 18:13:30 +0500

    Doctors, Doctors-inner page, About pages desktop

--- my_app.rb
+++ my_app.rb
@@ -13,3 +13,19 @@ end
 get "/our-clinics" do
 	erb :ourclinics
 end
+
+get "/our-clinics-2" do
+	erb :ourclinics_2
+end
+
+get "/doctors" do
+	erb :doctors
+end
+
+get "/dr-tariq" do
+	erb :drtariq
+end
+
+get "/about" do
+	erb :about
+end
